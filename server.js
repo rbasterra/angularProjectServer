eb8ef4dd0c004d41c5105617b3ef5ccc4dbe6354
Rafael Basterra Careaga <rbasterra@msn.com>
2022-03-14 19:39:05 +0100

    added secure cookie

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ server.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 3600000,
-        secure: false
+        secure: true,
+        sameSite: 'none'
     },
     store: MongoStore.create({mongoUrl: config.DB_URL})
 }));
